6e8e2a667f515c83d232a5d630b93effc452406a
kauker <kauker01@yandex.ru>
2015-08-07 12:35:46 +0300

    first commit of hello world for yii2

---
+++ views/site/say.php
@@ -0,0 +1,12 @@
+<?php
+
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+use yii\helpers\Html;
+?>
+<h1>Hello <?= Html::encode($target) ?></h1>
+<p>Welcome to your Yii2 demonstration application.</p>
